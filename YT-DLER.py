b74775a0e66841d63cfeb34f8ca5d5892c06d777
Suzu-merry <65783612+Suzu-merry@users.noreply.github.com>
2020-06-01 12:44:43 -0700

    Update YT-DLER.py
    Broke the one big ugly function into two slightly less ugly functions, fixed error handling in downloading

--- YT-DLER.py
+++ YT-DLER.py
@@ -7,29 +7,77 @@ from pytube import Playlist
 from pytube import YouTube
 import PySimpleGUI as sg
 
-def yt_url_dl(videoURL,audioOnly,outputFolder = None):
-    try:
-        video = YouTube(videoURL)
-        if audioOnly:
-            video.streams.filter(subtype='mp4', only_audio=True).first().download(filename=clean_filename(video.title), output_path=outputFolder)
-        else:
-            video.streams.get_highest_resolution().download(filename=clean_filename(video.title), output_path=outputFolder)
-        print(f'Successfully downloaded {video.title}')
-    except:
-        try:
-            print(f'Failed to download {video.title}... retrying...')
-            if audioOnly:
-                video.streams.filter(subtype='mp4', only_audio=True).first().download(filename=clean_filename(video.title), output_path=outputFolder)
-            else:
-                video.streams.get_highest_resolution().download(filename=clean_filename(video.title), output_path=outputFolder)
-            print(f'Successfully downloaded {video.title}')
-        except:
-            print(f'Failed to download {video.title}')
-            pass
-    
-def clean_filename(filename):
+#py -3 -m PyInstaller --onefile --hidden-import=PySimpleGui YT-DLER.py
+
+def sanitize(filename):
     return filename.translate(str.maketrans('', '', string.punctuation))
     
+def make_ordinal(n):
+    n = int(n)
+    suffix = ['th', 'st', 'nd', 'rd', 'th'][min(n % 10, 4)]
+    if 11 <= (n % 100) <= 13:
+        suffix = 'th'
+    return str(n) + suffix
+    
+def dlVideo(videoURL,audioOnly,outputFolder = None):
+    video = YouTube(videoURL)
+    videoTitle = sanitize(video.title)
+    if audioOnly:
+        video.streams.filter(subtype='mp4', only_audio=True).first().download(filename=videoTitle, output_path=outputFolder)
+    else:
+        video.streams.get_highest_resolution().download(filename=videoTitle, output_path=outputFolder)
+    return videoTitle
+    
+def processURL(target,audioOnly):
+    if 'playlist?' in target:
+        playlist = Playlist(target)
+        playlistName = sanitize(playlist.title())
+        print(f'Playlist: {playlistName}')
+        if not os.path.isdir(playlistName):
+            os.mkdir(playlistName)
+            print(f'Created folder for playlist: {playlistName}')
+        playlist_length = len(playlist.video_urls)
+        print("Videos in playlist:", playlist_length)
+        count = 0
+        failure_count = 0
+        while True:
+            try:
+                if count in range(playlist_length):
+                    videoURL = playlist.video_urls[count]
+                    result = dlVideo(videoURL,audioOnly,playlistName)
+                    print(f'Successfully downloaded {result}, {make_ordinal(count+1)} video from {playlistName}')
+                    count+=1
+                else:
+                    break
+            except:
+                try:
+                    print(f'Failed to download {make_ordinal(count+1)} video from{playlistName}, retrying...')
+                    result = dlVideo(videoURL,audioOnly,playlistName)
+                    print(f'Successfully downloaded {result}, {make_ordinal(count+1)} video from {playlistName}')
+                    count+=1
+                except:
+                    print(f'Failed to download {make_ordinal(count+1)} video from {playlistName}')
+                    count+=1
+                    failure_count+=1
+                    pass
+        print("Successful:", count-failure_count)
+        print("Failed:", failure_count)
+    else:
+        try:
+            result = dlVideo(target,audioOnly)
+            print(f'Successfully downloaded {result}')
+        except:
+            try:
+                print(f'Failed to download video\nretrying...')
+                result = dlVideo(target,audioOnly)
+                print(f'Successfully downloaded {result}')
+                count+=1
+            except:
+                print(f'Failed to download video')
+                count+=1
+                failure_count+=1
+                pass
+        
 sg.theme('Black')	
 main_label = sg.Text('Welcome to the YT-DLer')
 sub_label = sg.Text('Enter a YT URL:')
@@ -43,7 +91,7 @@ layout = [  [main_label],
             [audio_button, download_button, cancel_button] ]
 
 window = sg.Window('YT-DLer 1.0', layout)
-print('YT-DLer 1.0','fkn0wned.org','Ready...',sep='\n')
+print('YT-DLer 1.0','https://github.com/Suzu-merry/YT-DLER','Ready...',sep='\n')
 while True:
     event, values = window.read()
     if event in (None, 'Cancel'):
@@ -52,29 +100,7 @@ while True:
     textInput = values[0]
     audioOnly = values[1]
     if 'youtube.com' in textInput or 'youtu.be' in textInput:
-        if 'playlist?' in textInput:
-            playlist = Playlist(textInput)
-            playlistName = playlist.title().translate(str.maketrans('', '', string.punctuation))
-            print(f'Playlist: {playlistName}')
-            if not os.path.isdir(playlistName):
-                os.mkdir(playlistName)
-                print(f'Created folder for playlist: {playlistName}')
-            count = 0
-            print("Videos in playlist:", len(playlist.video_urls))
-            while True:
-                try:
-                    playlist_length = len(playlist.video_urls)
-                    if count in range(playlist_length):
-                        videoURL = playlist.video_urls[count]
-                        yt_url_dl(videoURL,audioOnly,playlistName)
-                        count+=1
-                    else:
-                        break
-                except:
-                    pass
-            count = 0
-        else:
-            yt_url_dl(textInput,audioOnly)
+        processURL(textInput,audioOnly)
     print('Ready for a new link!')
 window.close()
 print('boop!')
